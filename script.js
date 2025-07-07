fdf1ecdc652eb1413a841525a7d6ab8a83b4a1fd
willianlopeswla <willianlopeswla52@gmail.com>
2025-07-07 17:19:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+          document.getElementById('messageButton').addEventListener('click', function() {
+            document.getElementById('message').textContent = 'Seja bem-vindo(a) e tenha um ótimo dia!';
+        });
